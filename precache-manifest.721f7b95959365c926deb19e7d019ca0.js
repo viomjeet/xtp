93d1753747ec9f674c7419d1a3d75ace70e62c49
vikas kumar <viomjeet@gmail.com>
2020-07-31 22:12:50 +0530

    Updates

---
+++ precache-manifest.721f7b95959365c926deb19e7d019ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b5dce32673d1e69ccb7167552eafde",
+    "url": "/xtp/index.html"
+  },
+  {
+    "revision": "3341f2a214fa89f44cc6",
+    "url": "/xtp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b42f70df45ffb97e786d",
+    "url": "/xtp/static/css/main.4adfe5b5.chunk.css"
+  },
+  {
+    "revision": "3341f2a214fa89f44cc6",
+    "url": "/xtp/static/js/2.43818965.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/xtp/static/js/2.43818965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42f70df45ffb97e786d",
+    "url": "/xtp/static/js/main.abd62de4.chunk.js"
+  },
+  {
+    "revision": "e7a5b1580cebaaf90895",
+    "url": "/xtp/static/js/runtime-main.698439be.js"
+  }
+]);
